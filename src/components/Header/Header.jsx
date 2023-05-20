96689b051c454ac4209cc713169670ad694c4c5f
Nahuel <nahuetomas@outlook.com>
2023-05-20 13:58:51 -0300

    [UPDATE] Header - Styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,9 @@
+import './Header.css'
+
 export function Header() {
-  return <header>NCR</header>
+  return (
+    <header className="Header">
+      <h1 className="Header__title">NCR</h1>
+    </header>
+  )
 }
