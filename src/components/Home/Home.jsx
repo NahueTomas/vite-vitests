7d32915bc8275b8c0fb9860b9e7d222b7d8deabc
Nahuel <nahuetomas@outlook.com>
2023-05-20 15:34:15 -0300

    [UPDATE] Home - Fetching data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,69 @@
 import './Home.css'
+import { useEffect, useState } from 'react'
+
+// API
+import { getAccounts } from '../../services/api.service'
 
 // Components
 import { Title } from '../Title'
 import { Description } from '../Description'
+import { GridCard, GridCardSkeleton, GridCardError } from '../GridCard'
 
 export function Home() {
+  const [response, setResponse] = useState({
+    loading: true,
+    error: false,
+    accounts: [],
+  })
+
+  async function getData(controller) {
+    try {
+      const accounts = await getAccounts(controller.signal)
+      setResponse({
+        loading: false,
+        error: false,
+        accounts,
+      })
+    } catch (err) {
+      console.log(err)
+      setResponse({
+        loading: false,
+        error: true,
+        accounts: [],
+      })
+    }
+  }
+
+  useEffect(() => {
+    const controller = new AbortController()
+    getData(controller)
+
+    // Unmount the component
+    return () => {
+      // Abort the fetching
+      controller.abort()
+
+      // Reset data
+      setResponse({
+        loading: false,
+        error: false,
+        accounts: [],
+      })
+    }
+  }, [])
+
   return (
     <main className="Home">
       <section className="Home__info">
         <Description>Consulta de Saldo</Description>
         <Title>Selecciona la cuenta a consultar</Title>
+      </section>
+      <section>
+        {response.error && <GridCardError />}
+        {response.loading && <GridCardSkeleton />}
+        {response.accounts.length && !response.loading && (
+          <GridCard accounts={response.accounts} />
+        )}
       </section>
     </main>
   )
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,13 +1,13 @@
 export const URL = 'https://api.npoint.io/97d89162575a9d816661'
 
-export async function callAPI() {
-  const response = await fetch(URL)
+export async function callAPI(signal) {
+  const response = await fetch(URL, { signal })
   const data = await response.json()
 
   return data
 }
 
-export async function getAccounts() {
-  const data = await callAPI()
+export async function getAccounts(signal) {
+  const data = await callAPI(signal)
   return data.cuentas
 }
