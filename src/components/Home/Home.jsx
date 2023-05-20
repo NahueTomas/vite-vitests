63a0d8d2ac81341c2bdd87a28eeda2a488b81f7b
Nahuel <nahuetomas@outlook.com>
2023-05-20 14:40:47 -0300

    [UPDATE] Home - Add "Description" & "Title" components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,16 @@
 import './Home.css'
 
+// Components
+import { Title } from '../Title'
+import { Description } from '../Description'
+
 export function Home() {
-  return <main className="Home">Accounts</main>
+  return (
+    <main className="Home">
+      <section className="Home__info">
+        <Description>Consulta de Saldo</Description>
+        <Title>Selecciona la cuenta a consultar</Title>
+      </section>
+    </main>
+  )
 }
