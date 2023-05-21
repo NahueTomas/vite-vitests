e73e96872b1e4f705cc808a1aaa13c480eb4070f
Nahuel <nahuetomas@outlook.com>
2023-05-21 13:24:54 -0300

    [ADD] Page - AccountDetails

---
+++ src/pages/AccountDetails/AccountDetails.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+
+// Components
+import { Title } from '../../components/Title'
+import { Description } from '../../components/Description'
+
+// Helpers
+import { getAccountTypeBySymbol, getCurrencyBySymbol } from '../../helpers'
+
+export function AccountDetails({ saldo, tipo_letras, n, moneda }) {
+  return (
+    <main>
+      <section className="AccountDetails__info">
+        <Description>Consulta de saldo</Description>
+        <Title>Este es tu saldo actual</Title>
+      </section>
+
+      <section className="AccountDetails__data">
+        <p>Saldo de la cuenta: {saldo}</p>
+        <p>
+          Tipo de la cuenta: {getAccountTypeBySymbol(tipo_letras)} en{' '}
+          {getCurrencyBySymbol(moneda)}
+        </p>
+        <p>Número de la cuenta: {n}</p>
+      </section>
+    </main>
+  )
+}
+
+AccountDetails.propTypes = {
+  /** Account balance */
+  saldo: PropTypes.string.isRequired,
+  /** Account type */
+  tipo_letras: PropTypes.string.isRequired,
+  /** Number account */
+  n: PropTypes.string.isRequired,
+  /** Currency account */
+  moneda: PropTypes.string.isRequired,
+}
