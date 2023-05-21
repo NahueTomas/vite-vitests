3b0619f6dfe571985010725227019f94ac4b2069
Nahuel <nahuetomas@outlook.com>
2023-05-21 19:32:35 -0300

    [ADD] getAccountTypeByName - Tests

---
+++ __test__/helpers/accounts/getAccountTypeByName.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest'
+import { getAccountTypeByName } from '../../../src/helpers/accounts'
+
+describe('Test getAccountTypeByName', () => {
+  it('Should return "Cuenta Corriente" symbol', () => {
+    const currency = getAccountTypeByName('Cuenta Corriente')
+    expect(currency).toBe('CC')
+  })
+
+  it('Should return "Caja de Ahorro" symbol', () => {
+    const currency = getAccountTypeByName('Caja de Ahorro')
+    expect(currency).toBe('CA')
+  })
+
+  it('Should return "I dont know 1" symbol', () => {
+    const currency = getAccountTypeByName('I dont know 1')
+    expect(currency).toBe('CCP')
+  })
+
+  it('Should return "I dont know 2" symbol', () => {
+    const currency = getAccountTypeByName('I dont know 2')
+    expect(currency).toBe('Cc')
+  })
+})
