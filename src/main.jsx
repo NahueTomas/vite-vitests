a1b5bd1be71a7b863106ebcb53f8d07a30383b56
Nahuel <nahuetomas@outlook.com>
2023-05-21 14:53:24 -0300

    [REMOVE] StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
@@ -7,14 +6,14 @@ import { Home } from './pages/Home'
 import { Layout } from './components/Layout'
 
 // Contexts
-import { AccountsContextProvider } from './contexts'
+import { AccountsContextProvider, SelectedContextProvider } from './contexts'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Layout>
-    <AccountsContextProvider>
-      <Home />
-    </AccountsContextProvider>
-  </Layout>
-  // </React.StrictMode>
+  <SelectedContextProvider>
+    <Layout>
+      <AccountsContextProvider>
+        <Home />
+      </AccountsContextProvider>
+    </Layout>
+  </SelectedContextProvider>
 )
