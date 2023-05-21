f304fe2a94004ea1c89087ce53e6cd44cbfc025e
Nahuel <nahuetomas@outlook.com>
2023-05-21 00:01:47 -0300

    [UPDATE] Throw Error API

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -2,8 +2,8 @@ export const URL = 'https://api.npoint.io/97d89162575a9d816661'
 
 export async function callAPI(signal) {
   const response = await fetch(URL, { signal })
+  if (!response.ok) throw new Error('Hubo un error con la request')
   const data = await response.json()
-
   return data
 }
 
