a26239ea7e04e240449708f68e4c956355a38fc0
Nahuel <nahuetomas@outlook.com>
2023-05-22 04:07:35 -0300

    [FIX] JSdocs

--- src/helpers/accounts/getAccountTypeByName.js
+++ src/helpers/accounts/getAccountTypeByName.js
@@ -1,6 +1,7 @@
 import { getAccountTypes } from './getAccountTypes'
 
 /**
+ * Gets an account symbol by the account name
  *
  * @param {string} name - The name of the account type for which you want to get the symbol.
  * @returns {string} - The symbol associate with the account type. If there are no symbol associated with the account type, returns "No existe ese tipo de cuenta".
--- src/helpers/currencies/getCurrencyByName.js
+++ src/helpers/currencies/getCurrencyByName.js
@@ -1,6 +1,7 @@
 import { getCurrencies } from './getCurrencies'
 
 /**
+ * Gets the account name ffrom an account symbol
  *
  * @param {string} name - The name of the currency for which you want to get the symbol.
  * @returns {string} - The symbol associate with the currency. If there are no symbol associated with the currency, returns "Divisa no definida".
