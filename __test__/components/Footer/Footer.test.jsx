f041d2e8a98921f28af8fc15e2e53f26c979039c
Nahuel <nahuetomas@outlook.com>
2023-05-22 00:48:31 -0300

    [UPDATE] Footer - Tests

--- __test__/components/Footer/Footer.test.jsx
+++ __test__/components/Footer/Footer.test.jsx
@@ -1,5 +1,5 @@
-import { describe, expect, it } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, expect, it, vi } from 'vitest'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import { Footer } from '../../../src/components/Footer'
 import { SelectedContext } from '../../../src/contexts'
@@ -38,5 +38,30 @@ describe('Footer Tests', () => {
 
     const backButton = screen.queryByText('Salir')
     expect(backButton).toBeDefined()
+  })
+
+  it('Should call the function', () => {
+    const accountExample = {
+      e: '1',
+      n: '872378326701',
+      t: '02',
+      saldo: '1500',
+      moneda: 'u$s',
+      tipo_letras: 'CC',
+    }
+
+    const handleClick = vi.fn()
+
+    render(
+      <SelectedContext.Provider
+        value={{ selected: accountExample, setSelected: handleClick }}
+      >
+        <Footer />
+      </SelectedContext.Provider>
+    )
+
+    const backButton = screen.queryByText('Salir')
+    fireEvent.click(backButton)
+    expect(handleClick).toBeCalledTimes(1)
   })
 })
