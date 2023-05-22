b8efb9d864a1b5873f5cbde2743812ea8203c103
Nahuel <nahuetomas@outlook.com>
2023-05-22 04:58:25 -0300

    [FIX] Vite Build - Using relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '',
   test: {
     globals: true,
     environment: 'jsdom',
