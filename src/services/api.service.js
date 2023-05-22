a8b058f1f6817f8f7509eca8b462747dd125faf9
Nahuel <nahuetomas@outlook.com>
2023-05-22 00:00:44 -0300

    [FIX] Mock Service Worker - Signal

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,14 @@
 export const API_URL = 'https://api.npoint.io/97d89162575a9d816661'
 
 export async function callAPI(signal) {
-  const response = await fetch(API_URL, { signal })
+  let signalToSend = signal
+
+  // Dont send signal on TESTS (because there is an error with "Mock Service Worker")
+  // eslint-disable-next-line no-undef
+  if (typeof process !== 'undefined' && process.env.NODE_ENV === 'test')
+    signalToSend = undefined
+
+  const response = await fetch(API_URL, { signal: signalToSend })
   if (!response.ok) throw new Error('Hubo un error con la request')
   const data = await response.json()
   return data
