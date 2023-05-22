0a55514f457cc48e6a2de6e7d7fcddd4158ce35f
Nahuel <nahuetomas@outlook.com>
2023-05-22 01:53:58 -0300

    [UPDATE] Layout - Tests

--- __test__/components/Layout/Layout.test.jsx
+++ __test__/components/Layout/Layout.test.jsx
@@ -2,13 +2,57 @@ import { describe, expect, it } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
 import { Layout } from '../../../src/components/Layout'
-import { SelectedContext } from '../../../src/contexts'
+import { SelectedContext, AccountsContext } from '../../../src/contexts'
 
 describe('Layout Tests', () => {
+  const pages = [
+    [
+      {
+        e: '1',
+        n: '872378326701',
+        t: '02',
+        saldo: '1500',
+        moneda: 'u$s',
+        tipo_letras: 'CC',
+      },
+      {
+        e: '1',
+        n: '872378326702',
+        t: '01',
+        saldo: '-600',
+        moneda: '$',
+        tipo_letras: 'Cc',
+      },
+    ],
+  ]
+
+  const accounts = [
+    {
+      e: '1',
+      n: '872378326701',
+      t: '02',
+      saldo: '1500',
+      moneda: 'u$s',
+      tipo_letras: 'CC',
+    },
+    {
+      e: '1',
+      n: '872378326702',
+      t: '01',
+      saldo: '-600',
+      moneda: '$',
+      tipo_letras: 'Cc',
+    },
+  ]
+
   it('Should show the Header and Home ', () => {
     render(
       <SelectedContext.Provider value={{ selected: null }}>
-        <Layout />
+        <AccountsContext.Provider
+          value={{ pages, accounts, loading: false, error: false }}
+        >
+          <Layout />
+        </AccountsContext.Provider>
       </SelectedContext.Provider>
     )
 
@@ -17,6 +61,10 @@ describe('Layout Tests', () => {
 
     // Should show the Home
     expect(screen.getByText('Selecciona la cuenta a consultar')).toBeDefined()
+
+    // Should render the Home content
+    expect(screen.getByText(/872378326701/i)).toBeDefined()
+    expect(screen.getByText(/872378326702/i)).toBeDefined()
 
     // Does not show the back button
     const exitButton = screen.queryAllByText('Salir')
@@ -37,7 +85,11 @@ describe('Layout Tests', () => {
           },
         }}
       >
-        <Layout />
+        <AccountsContext.Provider
+          value={{ pages, accounts, loading: false, error: false }}
+        >
+          <Layout />
+        </AccountsContext.Provider>
       </SelectedContext.Provider>
     )
 
